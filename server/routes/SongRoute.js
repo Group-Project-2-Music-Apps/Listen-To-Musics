37865d9fb6c03fa2cfb4d2a0e4899f97d4a622cf
dewijoanne <dewijoanne97@gmail.com>
2020-10-28 18:09:33 +0700

    add song api, not finished

---
+++ server/routes/SongRoute.js
@@ -0,0 +1,48 @@
+const route = require('express').Router();
+
+const SongController = require('../controllers/SongController');
+const authentication = require('../middlewares/authentication');
+const axios = require('axios')
+
+route.use(authentication);
+route.get('/',SongController.findAll);
+route.post('/',SongController.create);
+route.delete('/:id',SongController.delete);
+
+
+
+route.get('/search/:track', (req, res, next) => {
+    const { track } = req.params
+    axios({
+        method: 'GET',
+        headers: {Authorization : '"'} ,
+        url: ``
+    })
+     .then(response => {
+         const musicData = response.data.tracks.items[0];
+         res.status(200).json({
+             musicData
+         })
+     })
+     .catch(err => next(err))
+})
+
+route.get('/:artist/:songname', (req, res, next) => {
+    const { artist, songname } = req.params
+    axios({
+        method: 'GET',
+        url: `https://api.lyrics.ovh/v1/${artist}/${songname}`
+    })
+     .then(response => {
+         const lyrics = response.data
+         res.status(200).json({
+             lyrics
+         })
+     })
+     .catch(err => next(err))
+})
+
+
+
+
+module.exports = route;
