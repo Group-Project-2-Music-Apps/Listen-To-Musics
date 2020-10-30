8aef9ac33623f59a54b492453d80bd77f1d49395
dewijoanne <dewijoanne97@gmail.com>
2020-10-30 16:43:20 +0700

    final

--- server/routes/SongRoute.js
+++ server/routes/SongRoute.js
@@ -15,7 +15,7 @@ route.get('/search/:track', (req, res, next) => {
     const { track } = req.params
     axios({
         method: 'GET',
-        headers: {Authorization : 'Bearer BQCdpmAEx3mUTRVxya4qPtNO83JR_-oAZWSx2ifGluoDrj7CZqQlK4sVvxX5bduUHwnQEox2bdQz-gbLVbfDD-RJWnAy4kG5XD22r-zaXUp_fPOG-jbIHZHbGzia3JF9ZM5S8UEGC-NNArzC7nME8zFu-F4lAiLhuPhlQ8ObrU0Pz08'} ,
+        headers: {Authorization : 'Bearer BQDIB-6NPPoO5nHrGuSftZGlSroSaRjMFyYeSVleKospCcbx3ld0O5jIvQiz084YRPdu3Eq0GwBcZkLf4G1bei-n0VjcJdSDyiB6_sqeqDbgf2SiWup5oWN3bDzhBG30wbZmjU_4jVmsA4amtfqHME-P9q3rVaawmijv15Rmb3BQKYY'} ,
         url: `https://api.spotify.com/v1/search?q=${track}&type=track&market=ID`
     })
      .then(response => {
